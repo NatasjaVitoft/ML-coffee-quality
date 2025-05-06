fca2d30cc633136d001ff60b2df1078a78f3a85a
NatasjaVitoft <cph-nn194@cphbusiness.dk>
2025-05-06 19:26:37 +0200

    prompt engineering

--- streamlit/app.py
+++ streamlit/app.py
@@ -5,17 +5,33 @@ import re
 
 API_URL = 'http://localhost:11434/api/chat'
 
-
+# Prompt engineering: https://arize.com/blog-course/mastering-openai-api-tips-and-tricks/
 
 def fetch_response(prompt: str) -> str:
 
+    traits = st.session_state.get("traits", "")
+    task = st.session_state.get("task", "")
+    tone = st.session_state.get("tone", "")
+    target = st.session_state.get("target", "")
+
+    system = (
+
+        f"Traits: {traits}\n"
+        f"Task: {task}\n"
+        f"Tone: {tone}\n"
+        f"Target: {target}"
+    )
+
+
     body = {
-        "model": "deepseek-r1:1.5b",  
-        "messages": [{
-            "role": "user", 
-            "content": prompt
-        }]
+        "model": "deepseek-r1:1.5b",
+        "messages": [
+            {"role": "system", "content": system,
+            },
+            {"role": "user", "content": prompt}
+        ]
     }
+
 
     try:
 
@@ -51,7 +67,26 @@ def fetch_response(prompt: str) -> str:
 
         return f"Error connecting to API"
 
-st.title("Deep Seek")
+# Sidebar with prompt settings 
+
+st.sidebar.title("Prompt engineering")
+
+with st.sidebar.form("form"):
+    
+    traits = st.text_area("Traits", "")
+    task = st.text_area("Task", "")
+    tone = st.text_area("Tone", "")
+    target = st.text_area("Target", "")
+
+    submitted = st.form_submit_button("Save")
+    
+    if submitted:
+        st.session_state["traits"] = traits
+        st.session_state["task"] = task
+        st.session_state["tone"] = tone
+        st.session_state["target"] = target
+        
+        st.sidebar.success("Settings have been saved")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
