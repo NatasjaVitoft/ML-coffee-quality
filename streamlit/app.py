3d607090352898877142780784dfa82fb59b8011
NatasjaVitoft <cph-nn194@cphbusiness.dk>
2025-05-06 12:48:22 +0200

    Deep seek

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,9 +1,57 @@
 import streamlit as st
+import requests
+import json
+import re
+
+API_URL = 'http://localhost:11434/api/chat'
+
+
 
 def fetch_response(prompt: str) -> str:
-    return "Hello I'm a cute bot!"
 
-st.title("Coffee Quality page")
+    body = {
+        "model": "deepseek-r1:1.5b",  
+        "messages": [{
+            "role": "user", 
+            "content": prompt
+        }]
+    }
+
+    try:
+
+        res = requests.post(API_URL, json=body)
+
+        if res.status_code == 200:
+
+            response_parts = res.text.strip().split("\n")
+
+            response = []
+
+            for part in response_parts:
+
+                try:
+                    response_json = json.loads(part)
+                    response.append(response_json['message']['content'])
+                except json.JSONDecodeError:
+                    continue
+
+            final_response = ''.join(response)
+
+            final_response = re.sub(r'<think>', '', final_response)
+            final_response = re.sub(r'</think>', '', final_response)
+
+            return final_response.strip()
+
+        
+        else:
+
+            return f"Error. Try again"
+    
+    except requests.exceptions.RequestException as e:
+
+        return f"Error connecting to API"
+
+st.title("Deep Seek")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
