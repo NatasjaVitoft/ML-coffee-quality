6122f46c5bc18712ec58a081da675fd91aa4b46d
kotteletfisk <ostekugle@gmail.com>
2025-05-08 09:36:21 +0200

    added upload feature

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 import json
 import re
+import pandas as pd
+
 
 API_URL = 'http://localhost:11434/api/chat'
 
@@ -13,24 +15,57 @@ def fetch_response(prompt: str) -> str:
     task = st.session_state.get("task", "")
     tone = st.session_state.get("tone", "")
     target = st.session_state.get("target", "")
+    desc_str = st.session_state.get("desc", "")
+    corr_str = st.session_state.get("corr", "")
+    sample_str = st.session_state.get("sample", "")
+
+    if isinstance(desc_str, pd.DataFrame) and isinstance(corr_str, pd.DataFrame) and isinstance(sample_str, pd.DataFrame):
+        desc_str = desc_str.to_csv(index=False)
+        corr_str = corr_str.to_csv(index=False)
+        sample_str = sample_str.to_csv(index=False)
+
 
     system = (
 
         f"Traits: {traits}\n"
         f"Task: {task}\n"
         f"Tone: {tone}\n"
-        f"Target: {target}"
+        f"Target: {target}\n"
+        f"Dataset Descriptive Statistics: {desc_str}\n"
+        f"Dataset Correlation Matrix: {corr_str}\n"
+        f"Dataset Sample: {sample_str}"
+            
     )
 
+    full_prompt = f"""
+You are provided a user prompt and a context.
+Use the context as a basis for answering the user prompt as good as you can.
+Respond acting as is specified in "Traits".
+Respond like youre addressing what is specified in "Target".
+
+Dataset information is provided as a Sample, Correlation Matrix and Descriptive Statistics.
+They represent a Pandas Dataframe.
+Use this as a background for responding.
+
+Only consider the context if it is provided explicitly
+
+Context:
+{system}
+
+User Prompt:
+{prompt}
+"""
 
     body = {
         "model": "deepseek-r1:1.5b",
         "messages": [
-            {"role": "system", "content": system,
-            },
-            {"role": "user", "content": prompt}
+           # {"role": "system", "content": system,
+           #},
+            {"role": "user", "content": full_prompt}
         ]
     }
+
+    st.write(body)
 
 
     try:
@@ -72,10 +107,21 @@ st.sidebar.title("Prompt engineering")
 
 with st.sidebar.form("form"):
     
-    traits = st.text_area("Traits", "")
-    task = st.text_area("Task", "")
-    tone = st.text_area("Tone", "")
-    target = st.text_area("Target", "")
+    traits = st.text_area("Traits", f'{st.session_state.get("traits", "")}')
+    task = st.text_area("Task", f'{st.session_state.get("task", "")}')
+    tone = st.text_area("Tone", f'{st.session_state.get("tone", "")}')
+    target = st.text_area("Target", f'{st.session_state.get("target", "")}')
+
+    uploaded_file = st.file_uploader("Clean Dataset")
+    
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        sample = df.sample(5)
+        desc = df.describe()
+        corr = df.corr()
+        st.dataframe(sample)
+        st.dataframe(desc)
+        st.dataframe(corr)
 
     submitted = st.form_submit_button("Save")
     
@@ -84,6 +130,9 @@ with st.sidebar.form("form"):
         st.session_state["task"] = task
         st.session_state["tone"] = tone
         st.session_state["target"] = target
+        st.session_state["desc"] = desc
+        st.session_state["corr"] = corr
+        st.session_state["sample"] = sample
         
         st.sidebar.success("Settings have been saved")
 
