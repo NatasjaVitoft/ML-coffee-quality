cc709aff3c9b6211e733ab0fc79ce64707ee0e9b
kotteletfisk <ostekugle@gmail.com>
2025-05-08 16:52:02 +0200

    update readme notes

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,8 +12,6 @@ API_URL = 'http://localhost:11434/api/chat'
 def fetch_response(prompt: str) -> str:
 
     traits = st.session_state.get("traits", "")
-    task = st.session_state.get("task", "")
-    tone = st.session_state.get("tone", "")
     target = st.session_state.get("target", "")
     desc_str = st.session_state.get("desc", "")
     corr_str = st.session_state.get("corr", "")
@@ -28,9 +26,6 @@ def fetch_response(prompt: str) -> str:
     full_prompt = f"""
 You are provided a user prompt and a context.
 Use the context as a basis for answering the user prompt as good as you can.
-Respond acting as is specified in "Traits".
-Respond like youre addressing what is specified in "Target".
-
 
 All dataframe information is provided in CSV format, but represents a Pandas Dataframe.
 Use this as a background for responding.
@@ -38,23 +33,23 @@ Use this as a background for responding.
 Only consider the context if it is provided explicitly
 
 ## Context:
-Traits: {traits}
-Task: {task}
-Tone: {tone}
-Target: {target}
+Act like you are:
+{traits}
+You should be addressing:
+{target}
 
 
 ### Dataset Sample
 If present, this is a 5 row sample of the users dataset:
 {sample_str}\n
 
-Dataset Descriptive Statitics
+### Dataset Descriptive Statitics
 This is generated from the users dataframe using pandas df.describe().
 the rows from top is: count, mean, standard deviation, mean, 25% quartile, 50% quartile
 , 75% quartile, max:
 {desc_str}\n
 
-Dataset Correlation Matrix.
+### Dataset Correlation Matrix.
 This is generated from the users dataframe using pandas df.corr()
 The rows represent the columns. the values is the correlation between features
 {corr_str}\n
@@ -115,9 +110,7 @@ st.sidebar.title("Prompt engineering")
 
 with st.sidebar.form("form"):
     
-    traits = st.text_area("Traits", f'{st.session_state.get("traits", "")}')
-    task = st.text_area("Task", f'{st.session_state.get("task", "")}')
-    tone = st.text_area("Tone", f'{st.session_state.get("tone", "")}')
+    traits = st.text_area("Traits and Tone", f'{st.session_state.get("traits", "")}')
     target = st.text_area("Target", f'{st.session_state.get("target", "")}')
 
     uploaded_file = st.file_uploader("Clean Dataset")
@@ -135,8 +128,6 @@ with st.sidebar.form("form"):
     
     if submitted:
         st.session_state["traits"] = traits
-        st.session_state["task"] = task
-        st.session_state["tone"] = tone
         st.session_state["target"] = target
         st.session_state["desc"] = desc
         st.session_state["corr"] = corr
